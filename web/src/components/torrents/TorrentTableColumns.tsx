c99d752fe22105583a3769e130172def5be5d5c1
smzase <3100335084@qq.com>
2026-05-04 10:31:48 +0800

    26w18f.3.3.4

--- web/src/components/torrents/TorrentTableColumns.tsx
+++ web/src/components/torrents/TorrentTableColumns.tsx
@@ -622,7 +622,7 @@ export const createColumns = (
                 style={{ width: `${row.original.progress * 100}%` }}
               />
             </div>
-            <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[11px] font-medium text-[#999] whitespace-nowrap">
+            <span className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-[11px] font-medium text-white whitespace-nowrap">
               {pct}%
             </span>
           </div>
